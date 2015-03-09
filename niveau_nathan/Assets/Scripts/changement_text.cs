a2899d2efa9049339f6be96e9ae4a0c70ec8fac9
nathan <nathangoyat@dbmail.com>
2015-03-09 22:27:19 +0100

    Le changement du texte marche dans le tutoriel

--- niveau_nathan/Assets/Scripts/changement_text.cs
+++ niveau_nathan/Assets/Scripts/changement_text.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class changement_text : MonoBehaviour {
-	public GUIText case_text;
+	public Text case_text;
 	public string text;
 	// Use this for initialization
-	void OnCollisionEnter()
+	void OnTriggerEnter()
 	{
 		case_text.text = text;
 	}
