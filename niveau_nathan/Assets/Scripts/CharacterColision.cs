2af12da50a11f91b87066a1ab90df2bb0a080070
nathan <nathangoyat@dbmail.com>
2015-03-10 11:47:23 +0100

    ajout de plateforme qui tombe dans prefab

--- niveau_nathan/Assets/Scripts/CharacterColision.cs
+++ niveau_nathan/Assets/Scripts/CharacterColision.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CharacterColision : MonoBehaviour {
 	public int index_niveau_suivant;
+	public GameObject plateforme_qui_tombe;
 	// Use this for initialization
 	void Start () {
 	
@@ -15,15 +16,15 @@ public class CharacterColision : MonoBehaviour {
 	void OnTriggerEnter(Collider other)
 	{
 		Debug.Log ("detection de triger");
-		if (other.gameObject.tag == "vide") 
-		{
-			Application.LoadLevel (Application.loadedLevel);
-			//Debug.Log("vide detecte");
-		} 
-		else if (other.gameObject.tag == "Finish") 
-		{
-			Debug.Log("détection fin niveau");
-			Application.LoadLevel(index_niveau_suivant);
-		}
+		if (other.gameObject.tag == "vide") {
+						Application.LoadLevel (Application.loadedLevel);
+						//Debug.Log("vide detecte");
+				} else if (other.gameObject.tag == "Finish") {
+						//Debug.Log("détection fin niveau");
+						Application.LoadLevel (index_niveau_suivant);
+				} else if (other.gameObject.tag == "plateformeTombante") {
+			Debug.Log("detection plateforme qui tombe");
+			plateforme_qui_tombe.rigidbody.isKinematic = false;
+				}
 	}
 }
