1be9bf13188abf51ad3f21b4fc83ed071d1056a4
nathan <nathangoyat@dbmail.com>
2015-03-29 17:18:35 +0200

    ajout de la boule

--- niveau_nathan/Assets/Scripts/CharacterColision.cs
+++ niveau_nathan/Assets/Scripts/CharacterColision.cs
@@ -8,23 +8,21 @@ public class CharacterColision : MonoBehaviour {
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 	void OnTriggerEnter(Collider other)
 	{
-		Debug.Log ("detection de triger");
-		if (other.gameObject.tag == "vide") {
+				Debug.Log ("detection de triger");
+				if (other.gameObject.tag == "vide") {
 						Application.LoadLevel (Application.loadedLevel);
 						//Debug.Log("vide detecte");
 				} else if (other.gameObject.tag == "Finish") {
 						//Debug.Log("détection fin niveau");
 						Application.LoadLevel (index_niveau_suivant);
 				} else if (other.gameObject.tag == "plateformeTombante") {
-			Debug.Log("detection plateforme qui tombe");
-			plateforme_qui_tombe.rigidbody.isKinematic = false;
+						Debug.Log ("detection plateforme qui tombe");
+						plateforme_qui_tombe.rigidbody.isKinematic = false;
+				} else if (other.gameObject.tag == "Ennemy") {
+						//Debug.Log("détection de la boule");
+						Application.LoadLevel (Application.loadedLevel);
 				}
 	}
 }
