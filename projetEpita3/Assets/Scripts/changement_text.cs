10f0c86a9326e1d7aa1b0438b00a5ec21f7c0185
nathan <nathangoyat@dbmail.com>
2015-04-02 13:21:18 +0200

    creation du chekpoint

---
+++ projetEpita3/Assets/Scripts/changement_text.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class changement_text : MonoBehaviour {
+	public Text case_text;
+	public string text;
+	// Use this for initialization
+	void OnTriggerEnter()
+	{
+		case_text.text = text;
+	}
+}
