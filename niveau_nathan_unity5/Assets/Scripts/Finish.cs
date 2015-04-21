53603020488baac4f245e723b9f186d547a1e60b
nathan <nathangoyat@dbmail.com>
2015-04-21 15:24:11 +0200

    optimisation du finish

---
+++ niveau_nathan_unity5/Assets/Scripts/Finish.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour {
+	public int next_level;
+	void OnTriggerEnter(Collider other)
+	{
+		Application.LoadLevel (next_level);
+	}
+}
