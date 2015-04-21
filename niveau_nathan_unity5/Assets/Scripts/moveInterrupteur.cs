19953ceb60c27ec593a0291e05b93bdadd060c6d
nathan <nathangoyat@dbmail.com>
2015-04-21 14:50:48 +0200

    perso avec anim et camera mais sans detection de souris

---
+++ niveau_nathan_unity5/Assets/Scripts/moveInterrupteur.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveInterrupteur : MonoBehaviour {
+	public GameObject objetActivable;
+
+	void OnCollisionEnter(Collision other)
+	{
+		Debug.Log ("colision detecter");
+		if (other.gameObject.tag == "Player"){
+			Debug.Log("colision détécté avec player");
+			this.GetComponent<Rigidbody>().isKinematic = true;
+			while (this.gameObject.transform.localScale.y >0.1)
+			{
+				this.gameObject.transform.localScale = new Vector3(0,((float)(0.1*Time.deltaTime)),0);
+				this.gameObject.transform.position = new Vector3(0,((float)(0.05*Time.deltaTime)),0);
+			}
+			this.gameObject.SetActive (false);
+			objetActivable.SetActive (true);
+		} 
+	}
+}
