d6539ed1f6c72c8497d656995f10d50750f2e529
nathan <nathangoyat@dbmail.com>
2015-06-27 11:28:08 +0200

    ajout script tex bouton

---
+++ script à remplacer/langue_text_button.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class langue_text_button
+{
+    public Text guitext;
+    public string fr_phrase;
+    public string en_sentence;
+
+    public void Start()
+    {
+        if (PlayerPrefs.GetInt("language", 0) == 0)
+        {
+            guitext.text = fr_phrase;
+        }
+        else
+        {
+            guitext.text = en_sentence;
+        }
+    }
+}
